123790dc912bcc44a2ed7e4c4a94766e8b6fcd62
MakeCodeKraus <77696607+MakeCodeKraus@users.noreply.github.com>
2023-02-27 09:04:49 -0500

    inicio de la actividad

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require("path");
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+   
+  const validarSiElUsuarioYaEstaConectado = (username)=>{
+    return true;
+  }
+  //Por Resolver
+  // io.use((socket,next)=>{
+  //    const username = socket.handshake.query.username;
+  //   if(!username || validarSiElUsuarioYaEstaConectado(username)){
+  //     socket.emit("connectionRejected");
+  //     return next(new Error("El usuario ya esta conectado"))
+  //   }
+  //   else{
+  //     socket.username = username;
+  //     next();
+  //   }
+  // })
+
+ io.on('connection', (socket)=>{
+   const username = socket.handshake.query.username;
+   if(!username || validarSiElUsuarioYaEstaConectado(username)){
+       socket.emit("connectionRejected");
+       socket.disconnect();
+     }
+     
+     console.log(username+' connected');
+  
+      socket.on('disconnect', () => {
+        console.log( username+' disconnected');
+      });
+ } ) 
+
+
+  server.listen(3000, () => {
+    console.log('listening on *:3000');
+  }); 
